50cc81581778534e7e4e6f50b2b241faa656502e
Oladehinde Kazeem <larrysnet2001@gmail.com>
2019-12-27 08:23:13 +0100

    ch(redux): setup redux
    - install redux and necessary dependencies
    - setup redux store
    
    [Maintains]

---
+++ src/store/types/authTypes.js
@@ -0,0 +1,5 @@
+export const LOGIN = 'LOGIN';
+export const AUTH_FETCH = 'AUTH_FETCH';
+export const AUTH_RESOLVE = 'AUTH_RESOLVE';
+export const AUTH_REJECT = 'AUTH_REJECT';
+export const AUTH_CANCEL = 'AUTH_CANCEL';
